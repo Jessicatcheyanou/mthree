35cffa610ea4f68f5f5b3c57c6292840c94939c1
Jessicatcheyanou <jessicatcheyanou@gmail.com>
2022-02-25 21:21:55 -0500

    Jess|Hello World

---
+++ src/main/java/JavaBasics/HelloWorld.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Jessica Tcheyanou");
+        System.out.println("I live in Quebec City,Quebec,Canada");
+    }
+}
+
